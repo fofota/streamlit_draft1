3e7fec2213760dd557259f667c50356aa88b92eb
Pedro Peçanha <pedrompecanha27@gmail.com>
2024-01-27 12:38:50 -0300

    Adding new changes, including sorting by position initially and better player page display.

--- app3.py
+++ app3.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import pandas as pd
 
 app = Flask(__name__)
@@ -6,13 +6,37 @@ app = Flask(__name__)
 # Read the CSV file into a pandas DataFrame
 full_df = pd.read_csv('parsed_data.csv')
 
+
 @app.route('/')
 def index():
+    global full_df
+    def map_position(position):
+        if position.startswith('GK'):
+            return 1
+        elif position.startswith('DM'):
+            return 3
+        elif position.startswith('WB'):
+            return 2.5  # You can use any value between 2 and 3
+        elif position.startswith('D'):
+            return 2
+        elif position.startswith('M'):
+            return 4
+        elif position.startswith('A'):
+            return 5
+        elif position.startswith('S'):
+            return 6
+        else:
+            return 7
+    # Add a new column 'MappedPosition' based on the mapping function
+    full_df['MappedPosition'] = full_df['Position'].apply(map_position)
+
+    # Sort the DataFrame by 'MappedPosition'
+    
     # Select specific columns
-    selected_columns = ['Inf', 'Name', 'Age', 'Wage', 'Transfer Value', 'Nat', 'Position', 'Personality', 'Av Rat', 'Mins', 'Gls', 'Ast', 'NP-xG/90', 'xA/90']
+    selected_columns = ['MappedPosition','Inf', 'Name', 'Age', 'Wage', 'Transfer Value', 'Nat', 'Position', 'Personality', 'Av Rat', 'Mins', 'Gls', 'Ast', 'NP-xG/90', 'xA/90']
     df = full_df[selected_columns]
 
-    return render_template('index.html', table=df)
+    return render_template('index.html', table=df.sort_values(by='MappedPosition'))
 
 @app.route('/player/<name>')
 def player_page(name):
